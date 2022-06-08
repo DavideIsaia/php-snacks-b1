83881d1ddb13795303f16bd262d1c1d0695a5eaa
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-08 14:31:17 +0200

    snack 2 funzionante

--- snack2/index.php
+++ snack2/index.php
@@ -7,6 +7,22 @@
   <title>PHP Snack 2</title>
 </head>
 <body>
-<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+  <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+  <?php
+    $name = $_GET["name"];
+    $mail = $_GET["mail"];
+    $age = $_GET["age"];
+    $mail_at = strpos($mail, "@");
+    $mail_dot = strpos($mail, ".");
+  ?>
+
+  <?php if (strlen($name) >= 3 && $mail_at !== false && $mail_dot !== false  && is_numeric($age)) { ?>
+    <div>
+      <h2>Accesso riuscito!</h2>
+      <!-- ad es http://localhost/php-snacks-b1/snack2/?name=pippo&mail=pippo@mail.it&age=30 -->
+    </div>
+  <?php } else { ?>
+      <h2>Accesso negato :(</h2>
+  <?php } ?>
 </body>
 </html>
