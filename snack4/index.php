cbaff108b84da36e1a3b215385a1b21ce848d845
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-08 16:18:54 +0200

    snack 4 completato

--- snack4/index.php
+++ snack4/index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Snack 4</title>
 </head>
+
 <body>
-<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+  <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+  <?php
+  $randoms = [];
+  while (count($randoms) < 15) {
+    $element = rand(1, 50);
+    if (!in_array($element, $randoms)) {
+      $randoms[] = $element;
+    }
+  }
+  ?>
+  <h3><?php echo join(", ",$randoms); ?></h3>
 </body>
+
 </html>
